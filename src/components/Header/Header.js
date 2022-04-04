4569104779f1c688a221ae98e905c27c814ef02a
Dev-Zual <freelancerzual@gmail.com>
2022-04-04 21:35:11 +0600

    added active linkt

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css';
 const Header = () => {
   return (
     <div className="mt-7 text-center">
-      <nav>
-        <Link
+      <nav className="flex justify-center">
+        <CustomLink
           className="ml-4 duration-300 hover:text-red-500 py-2 px-3 uppercase font-bold"
           to="/"
         >
           Home
-        </Link>
-        <Link
+        </CustomLink>
+        <CustomLink
           className="ml-4 py-2 duration-300 hover:text-red-500 px-3 uppercase font-bold"
           to="/reviews"
         >
           Reviews
-        </Link>
-        <Link
+        </CustomLink>
+        <CustomLink
           className="ml-4 py-2 duration-300 px-3 hover:text-red-500 uppercase font-bold"
           to="/dashboard"
         >
           Dashboard
-        </Link>
-        <Link
+        </CustomLink>
+        <CustomLink
           className="ml-4 py-2 duration-300 hover:text-red-500 px-3 uppercase font-bold"
           to="/blog"
         >
           Blog
-        </Link>
-        <Link
+        </CustomLink>
+        <CustomLink
           className="ml-4 py-2 duration-300 hover:text-red-500 px-3 uppercase font-bold"
           to="/about"
         >
           About
-        </Link>
+        </CustomLink>
       </nav>
     </div>
   );
