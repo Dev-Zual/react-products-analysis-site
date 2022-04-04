1d3dac7022383d36d07700652c42fe7241027cd6
Dev-Zual <freelancerzual@gmail.com>
2022-04-04 12:23:05 +0600

    create slider area

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h3>this is home</h3>
+    <div className="grid sm:grid-cols-1 md:grid-cols-12 mt-10">
+      <div className="slider-text md:col-span-7">
+        <h1>Your Best Apple </h1>
+        <h1>Laptop Is Here</h1>
+      </div>
+      <div className="slider-img md:col-span-5">
+        <img
+          className="w-80"
+          src="https://www.bdstall.com/asset/product-image/giant_57855.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
