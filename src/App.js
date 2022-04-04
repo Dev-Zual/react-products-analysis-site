a3ee9323d796408e42f0bbefa064fa0cfbaa452f
Dev-Zual <freelancerzual@gmail.com>
2022-04-04 11:38:40 +0600

    setup route in app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <h1>assignment 9 started.</h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+      </Routes>
     </div>
   );
 }
